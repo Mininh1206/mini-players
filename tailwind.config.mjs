07a2ac274b074bd771e85ad6f5beaca92fcd267b
Daniel <danielmarchenajimenez2002@gmail.com>
2024-03-17 21:20:19 +0100

    Menu and game component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#3C486B',
+      'secondary': '#F0F0F0',
+      'tertiary': '#F9D949',
+      'accent': '#F45050',
+    }
+  },
+  plugins: [require("flowbite/plugin")],
+};
